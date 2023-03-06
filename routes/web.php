9f9f2315943aac2afa630b5f78930416b0237ba9
ysm-A <yuto950910@gmail.com>
2023-03-06 08:20:51 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
-Route::get('/',[WelcomeController::class,'index']);
+Route::get('/welcome',[WelcomeController::class,'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
