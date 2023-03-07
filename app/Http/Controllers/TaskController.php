ce94b055a0178c23d764f36b30185521f48e2472
ysm-A <yuto950910@gmail.com>
2023-03-07 15:38:38 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,12 +16,16 @@ class TaskController extends Controller
      */
     public function list()
     {
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 2;
+        
         // 一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
                          ->orderBy('priority', 'DESC')
                          ->orderBy('period')
                          ->orderBy('created_at')
-                         ->get();
+                         ->paginate($per_page);
+                        // ->get();
 /*
 $sql = TaskModel::where('user_id', Auth::id())
                  ->orderBy('priority', 'DESC')
--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -5,7 +5,7 @@
 
 {{-- メインコンテンツ --}}
 @section('contets')
-        <h1>タスクの登録</h1>
+        <h1>タスクの登録(未実装)</h1>
             @if (session('front.task_register_success') == true)
                 タスクを登録しました！！<br>
             @endif
@@ -41,14 +41,29 @@
             <td>{{ $task->getPriorityString() }}
             <td><a href="./detail.html">詳細閲覧</a>
             <td><a href="./edit.html">編集</a>
-            <td><form action="./top.html"><button>完了</button></form>
+            <td><form action="./top.html"><button>完了</button></form></a>
 @endforeach
         </table>
         <!-- ページネーション -->
-        現在 1 ページ目<br>
-        <a href="./top.html">最初のページ(未実装)</a> / 
-        <a href="./top.html">前に戻る(未実装)</a> / 
-        <a href="./top.html">次に進む(未実装)</a>
+        {{-- {{ $list->links() }} --}}
+        現在 {{ $list->currentPage() }} ページ目<br>
+        @if ($list->onFirstPage() === false)
+        <a href="/task/list">最初のページ</a>
+        @else
+        最初のページ
+        @endif
+        /
+        @if ($list->previousPageUrl() !== null)
+            <a href="{{ $list->previousPageUrl() }}">前に戻る</a>
+        @else
+            前に戻る
+        @endif
+        /
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        @else
+            次に進む
+        @endif
         <br>
         <hr>
         <menu label="リンク">
