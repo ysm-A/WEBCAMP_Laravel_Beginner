49a7c5988bf082369daa0208d7dbb4babc6c9ddb
ysm-A <yuto950910@gmail.com>
2023-03-18 07:43:14 +0000

    会員登録完成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRegisterPost;
 use Illuminate\Support\Facades\Auth;
-use App\Models\users as usersModel;
+use App\Models\User as usersModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
